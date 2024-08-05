d2d926c58441e1b7408c9ade8785d3458344b597
HarishManeri <maneriharish21@gmail.com>
2024-08-05 15:47:04 +0530

    Create server controller.jsx

---
+++ server controller.jsx
@@ -0,0 +1,23 @@
+const Restaurant = require('../models/Restaurant');
+
+exports.getAllRestaurants = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getRestaurantById = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findById(req.params.id);
+    if (restaurant) {
+      res.json(restaurant);
+    } else {
+      res.status(404).json({ message: 'Restaurant not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
